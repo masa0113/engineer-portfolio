f2c19959d72a01a34b637cd5c9a92c41534cd885
poison <masahiro.fukuyama@roxx.co.jp>
2025-01-17 18:03:44 +0900

    add style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="ja" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-700`}
       >
         {children}
       </body>
