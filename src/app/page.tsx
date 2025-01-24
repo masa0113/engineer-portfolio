b4d6517d033bff52c48caff9a80dc23d6d0dbb57
poison <masahiro.fukuyama@roxx.co.jp>
2025-01-24 15:43:54 +0900

    delete: APIを叩く箇所を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import HomeWrapper from '@/components/top/HomeWrapper';
-import { getPR } from '@/lib/cms';
 
 export default function Home() {
-  getPR().then((v) => console.log(v));
   return <HomeWrapper />;
 }
