93fbfdcc85c1264d11d69ae327c5ef19ac7f0df2
Fukuyama a.k.a poison <69804084+masa0113@users.noreply.github.com>
2025-01-31 22:46:26 +0900

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,12 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 import type { Configuration } from 'webpack';
 
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
     domains: [
       'images.microcms-assets.io', // microCMSの画像を使用する場合
     ],
-  }, // GitHub Pages に合わせた画像最適化設定
-  basePath: '/engineer-portfolio', // GitHub Pages のリポジトリ名
-  assetPrefix: '/engineer-portfolio', // 静的ファイルのパス修正
+  },
   // bunの使用時に必要な設定
   experimental: {
     esmExternals: true,
